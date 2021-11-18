3b62d35cc69f98f512d93c223880002605ac3b29
Leonardo Gurgel <lgurgel@id.uff.br>
2021-11-18 16:49:01 -0300

    rename project

---
+++ src/main/java/com/example/sispre/SispreApplication.java
@@ -0,0 +1,13 @@
+package com.example.sispre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SispreApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SispreApplication.class, args);
+  }
+
+}
