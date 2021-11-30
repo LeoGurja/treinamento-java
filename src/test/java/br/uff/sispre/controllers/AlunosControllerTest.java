a4bd1ccb7c941d36955bc14ad5a10c38e3e7f052
Leonardo Gurgel <lgurgel@id.uff.br>
2021-11-30 14:11:31 -0300

    fix alunosController tests

--- src/test/java/br/uff/sispre/controllers/AlunosControllerTest.java
+++ src/test/java/br/uff/sispre/controllers/AlunosControllerTest.java
@@ -9,10 +9,14 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.uff.sispre.controllers.resources.AlunoResource;
+import br.uff.sispre.helpers.Sanitizer;
+import br.uff.sispre.helpers.Sha256;
+import br.uff.sispre.models.Aluno;
 import br.uff.sispre.repositories.AlunoRepository;
 
 @SpringBootTest
@@ -25,8 +29,8 @@ public class AlunosControllerTest {
   @Autowired
   private ObjectMapper objectMapper;
 
-  // @Autowired
-  // private AlunoRepository repo;
+  @Autowired
+  private AlunoRepository repo;
 
   @Test
   void criaAlunoValido() throws Exception {
@@ -38,8 +42,11 @@ public class AlunosControllerTest {
     aluno.password = "Senha_segura";
     aluno.email = "carlinhos@email.com";
     aluno.phoneNumber = "(11)11111-1111";
+
     mvc.perform(MockMvcRequestBuilders.post("/alunos").contentType("application/json")
         .content(objectMapper.writeValueAsString(aluno))).andExpect(status().isOk());
+
+    compare(repo.findByCpf("11111111111"), aluno);
   }
 
   @Test
@@ -47,5 +54,15 @@ public class AlunosControllerTest {
     AlunoResource aluno = new AlunoResource();
     mvc.perform(MockMvcRequestBuilders.post("/alunos").contentType("application/json")
         .content(objectMapper.writeValueAsString(aluno))).andExpect(status().isBadRequest());
+  }
+
+  private void compare(Aluno aluno, AlunoResource alunoResource) {
+    assertEquals(aluno.getAddress(), alunoResource.address);
+    assertEquals(aluno.getCpf(), Sanitizer.sanitize(Sanitizer.cpf, alunoResource.cpf));
+    assertEquals(aluno.getRg(), Sanitizer.sanitize(Sanitizer.rg, alunoResource.rg));
+    assertEquals(aluno.getPhoneNumber(), Sanitizer.sanitize(Sanitizer.phoneNumber, alunoResource.phoneNumber));
+    assertEquals(aluno.getEmail(), alunoResource.email);
+    assertEquals(aluno.getName(), alunoResource.name);
+    assertEquals(aluno.getPasswordDigest(), Sha256.encryptPassword(alunoResource.password));
   }
 }
