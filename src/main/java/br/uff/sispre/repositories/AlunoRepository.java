fe3f44462161d761c3653102f91dd3b4ddeaccce
Leonardo Gurgel <lgurgel@id.uff.br>
2021-11-30 14:11:07 -0300

    add findByCpf

--- src/main/java/br/uff/sispre/repositories/AlunoRepository.java
+++ src/main/java/br/uff/sispre/repositories/AlunoRepository.java
@@ -3,5 +3,5 @@ package br.uff.sispre.repositories;
 import br.uff.sispre.models.Aluno;
 
 public interface AlunoRepository extends PessoaRepository<Aluno> {
-
+  Aluno findByCpf(String cpf);
 }
--- src/main/java/br/uff/sispre/repositories/ProfessorRepository.java
+++ src/main/java/br/uff/sispre/repositories/ProfessorRepository.java
@@ -3,5 +3,5 @@ package br.uff.sispre.repositories;
 import br.uff.sispre.models.Professor;
 
 public interface ProfessorRepository extends PessoaRepository<Professor> {
-
+  Professor findByCpf(String cpf);
 }
