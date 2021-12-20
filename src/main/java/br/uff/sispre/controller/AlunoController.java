7fd4efd7be19c61f7f155f1878d831599ebe355f
Leonardo Gurgel <lgurgel@id.uff.br>
2021-12-20 18:49:58 -0300

    add alunos/show

--- src/main/java/br/uff/sispre/controller/AlunoController.java
+++ src/main/java/br/uff/sispre/controller/AlunoController.java
@@ -1,14 +1,28 @@
 package br.uff.sispre.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import br.uff.sispre.service.AlunoService;
 
 @Controller
 @RequestMapping("/alunos")
 public class AlunoController {
-  @GetMapping(path = "/")
-  public String index() {
+  @Autowired
+  private AlunoService alunoService;
+
+  @GetMapping("/")
+  public String index(Model model) {
+    model.addAttribute("alunos", alunoService.all());
     return "alunos/index";
+  }
+
+  @GetMapping("/{id}")
+  public String show(@PathVariable Long id, Model model) {
+    model.addAttribute("aluno", alunoService.find(id));
+    return "alunos/show";
   }
 }
